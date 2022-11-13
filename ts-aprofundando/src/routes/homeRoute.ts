f2837e36b4a1c3b3c251263c5f7f9280f362a80e
Aron Ricardo <aronricardo@hotmail.com>
2022-11-13 02:02:40 -0300

    end

--- ts-aprofundando/src/routes/homeRoute.ts
+++ ts-aprofundando/src/routes/homeRoute.ts
@@ -1,39 +1,17 @@
 import { Router, Request, Response } from 'express'
+import * as homeController from '../controllers/homeController'
+import * as infoController from '../controllers/infoController'
+import * as userController from '../controllers/userController'
 
 const router = Router()
 
-router.get('/home', (req:Request, res:Response) => {
-    
-    let usuario = {
-        nome: "Isis",
-        idade: 1
-    }
+router.get('/home', homeController.home)
 
-    let maiorIdade: boolean = (usuario.idade >= 18) ? true : false
+router.get("/contato", infoController.contato)
+router.get("/sobre", infoController.sobre)
 
-    res.render('pages/home', {
-        user: "Aron", //1º parametro: nome do view(arquivo html). 2º parametro: envio da variárel para o view
-        usuario,
-        maiorIdade,
-        products: [
-            {title : 'Produto X', price: 10},
-            {title : 'Produto Y', price: 15},
-            {title : 'Produto W', price: 25}
-        ],
-        frasesDoDia: [
-            'Jesus é Bom!',
-            'Eu sou baterista de Cristo',
-            'Nossa família é abençoada! '
-        ]
-    }) 
-})
-
-router.get("/contato", (req:Request, res:Response) => {
-    res.render('pages/contato')
-})
-
-router.get("/sobre", (req:Request, res:Response) => {
-    res.render('pages/sobre')
-})
+router.get('/nome', userController.nome)
+router.get('/idade', userController.idadeForm)
+router.post('/idade', userController.idadeAction)
 
 export default router
--- ts-aprofundando/src/server.ts
+++ ts-aprofundando/src/server.ts
@@ -1,9 +1,12 @@
 import express,{Request, Response} from 'express' //importa todas as funções do express
 import mustache from 'mustache-express'
 import path from 'path'
+import dotenv from 'dotenv'
 import routeIndex from './routes/contatoRoute'
 import routeNoticia from './routes/noticiasRoute'
 import routeHome from './routes/homeRoute'
+
+dotenv.config()
 
 const server  = express()
 
@@ -11,9 +14,10 @@ server.set('view engine', 'mustache')  //indica as configurações que o templat
 server.set('views', path.join(__dirname, 'views'))  //indica aonde fica os arquivos em html do projeto
 server.engine('mustache', mustache()) //define a engine, que no caso é o mustache e registra no express
 
+server.use(express.urlencoded({extended: true}))//habilitando a coleta de dados detalhados via POST no corpo da requisição.
 server.use(express.static(path.join(__dirname, '../public'))) //tornou a pasta public estática
 server.use("/informacoes",routeIndex) //prefixo "/contrato"(ñ obrigatório)
 server.use("/noticia",routeNoticia) //prefixo "/noticia"(ñ obrigatório)
 server.use(routeHome) 
 
-server.listen(3000, () => console.log("Server running..."))
+server.listen(process.env.PORT, () => console.log("Server running..."))
